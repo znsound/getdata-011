438014bbbc49f40f634713c44fc64f6f625fe803
znsound <znsound@gmail.com>
2015-02-22 15:16:22 +0900

    code change

--- run_analysis.R
+++ run_analysis.R
@@ -9,35 +9,35 @@ mySfx <- function(x, sfx) {
 }
 
 require(plyr)
-pathfile<-file.path(getwd(),"UCI HAR Dataset")
-testFilePath<-file.path(pathfile, "test")
-trainFilePath<-file.path(pathfile, "train")
-xtest<-read.table(file.path(testFilePath,"X_test.txt"))
-ytest<-read.table(file.path(testFilePath,"Y_test.txt"))
-subjecttest<-read.table(file.path(testFilePath,"subject_test.txt"))
-xtrain<-read.table(file.path(trainFilePath,"X_train.txt"))
-ytrain<-read.table(file.path(trainFilePath,"Y_train.txt"))
-subjecttrain<-read.table(file.path(trainFilePath,"subject_train.txt"))
-activityLabels<-read.table(file.path(pathfile, "activity_labels.txt"), col.names = c("Id", "Activity"))
-featurelabels<-read.table(file.path(pathfile, "features.txt"),colClasses = c("character"))
+pathfile <- file.path(getwd(),"UCI HAR Dataset")
+testFilePath <- file.path(pathfile, "test")
+trainFilePath <- file.path(pathfile, "train")
+xtest <- read.table(file.path(testFilePath,"X_test.txt"))
+ytest <- read.table(file.path(testFilePath,"Y_test.txt"))
+subjectTest <- read.table(file.path(testFilePath,"subject_test.txt"))
+xtrain <- read.table(file.path(trainFilePath,"X_train.txt"))
+ytrain <- read.table(file.path(trainFilePath,"Y_train.txt"))
+subjectTrain <- read.table(file.path(trainFilePath,"subject_train.txt"))
+activityLabels <- read.table(file.path(pathfile, "activity_labels.txt"), col.names = c("Id", "Activity"))
+featureLabels <- read.table(file.path(pathfile, "features.txt"),colClasses = c("character"))
 
-traindata<-cbind(cbind(xtrain, subjecttrain), ytrain)
-testdata<-cbind(cbind(xtest, subjecttest), ytest)
-sensordata<-rbind(traindata, testdata)
-sensorlabels<-rbind(rbind(featurelabels, c(562, "Subject")), c(563, "Id"))[,2]
-names(sensordata)<-sensorlabels
+trainData <- cbind(cbind(xtrain, subjectTrain), ytrain)
+testData <- cbind(cbind(xtest, subjectTest), ytest)
+sensorData <- rbind(trainData, testData)
+sensorLabels <- rbind(rbind(featureLabels, c(562, "Subject")), c(563, "Id"))[,2]
+names(sensorData) <- sensorLabels
 
-sensordatameanstd <- sensordata[,grepl("mean\\(\\)|std\\(\\)|Subject|Id", names(sensordata))]
+sensorDataMeanStd <- sensorData[,grepl("mean\\(\\)|std\\(\\)|Subject|Id", names(sensorData))]
 
-sensordatameanstd <- join(sensordatameanstd, activityLabels, by = "Id", match = "first")
-sensordatameanstd <- sensordatameanstd[,-1]
+sensorDataMeanStd <- join(sensorDataMeanStd, activityLabels, by = "Id", match = "first")
+sensorDataMeanStd <- sensorDataMeanStd[,-1]
 
-names(sensordatameanstd) <- gsub("([()])","",names(sensordatameanstd))
-names(sensordatameanstd) <- make.names(names(sensordatameanstd))
+names(sensorDataMeanStd) <- gsub("([()])","",names(sensorDataMeanStd))
+names(sensorDataMeanStd) <- make.names(names(sensorDataMeanStd))
 
-finaldata<-ddply(sensordatameanstd, c("Subject","Activity"), numcolwise(mean))
-finaldataheaders<-names(finaldata)
-finaldataheaders<-sapply(finaldataheaders, mySfx, ".mean")
-names(finaldata)<-finaldataheaders
+finalData <- ddply(sensorDataMeanStd, c("Subject","Activity"), numcolwise(mean))
+finalDataHeaders <- names(finalData)
+finalDataHeaders <- sapply(finalDataHeaders, mySfx, ".mean")
+names(finalData) <- finalDataHeaders
 
-write.table(finaldata, file = "result.txt", row.name=FALSE)
+write.table(finalData, file = "result.txt", row.name=FALSE)
